996ca45891e5bfc6317a01fc78bafab5431574ec
Grzegorz Kielar <grzegorz.kielar@kielniakodu.io>
2023-08-14 12:46:53 +0200

    Manul mock

--- tests/Model/Basket.test.js
+++ tests/Model/Basket.test.js
@@ -1,7 +1,14 @@
 import Basket from "../../src/Model/Basket";
 import Product from "../../src/Model/Product";
 
-jest.mock('../../src/Model/Product');
+jest.mock('../../src/Model/Product', () => {
+	return jest.fn().mockImplementation(() => {
+		return {
+			getVat: jest.fn().mockReturnValue(0.23)
+			//getVat: jest.fn().mockReturnValueOnce(0.23).mockReturnValueOnce(1) //first and second execute
+		}
+	})
+});
 
 beforeEach(() => {
 	Product.mockClear();
@@ -11,6 +18,8 @@ it('Check total vat', () => {
 	const basket = new Basket();
 	const product = new Product('Test', 500);
 	basket.addProduct(product);
-	basket.getTotalVat()
-	expect(Product.mock.instances[0].getVat).toHaveBeenCalledTimes(1);
+	basket.addProduct(product);
+	expect(basket.getTotalVat()).toEqual(0.46);
+	//expect(Product.mock.instances[0].getVat).toHaveBeenCalledTimes(1);
+	expect(jest.spyOn(product, 'getVat')).toHaveBeenCalledTimes(2);
 });
